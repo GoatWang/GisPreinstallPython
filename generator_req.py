d26bd4890552c5beda9892b687c1949bcf6c218e
GoatWang <jeremy4555@yahoo.com.tw.com>
2019-08-10 12:30:39 +0800

    first

---
+++ generator_req.py
@@ -0,0 +1,21 @@
+import os
+
+write_str = """"""
+with open("requirements.txt", 'r' , encoding="utf8") as f:
+    for line in f:
+        if not (
+            "Fiona" in line or 
+            "GDAL" in line or 
+            "geopandas" in line or 
+            # "opencv" in line or
+            "Rtree" in line or 
+            "Shapely" in line
+            ):
+            write_str += line
+
+# write_str += "--index-url http://rd.thinktronltd.com:28181/simple --trusted-host rd.thinktronltd.com\n"
+# write_str += "PySaga"
+
+
+with open("requirements_new.txt", 'w' , encoding="utf8") as f:
+    f.write(write_str)
---
+++ generator_req_pip_server.py
@@ -0,0 +1,24 @@
+import os
+
+package_names = [
+    "Fiona", 
+    "GDAL",  
+    "Rtree", 
+    "Shapely", 
+    "geopandas",
+    # "SplittedImage", 
+    # "PySaga"
+]
+
+write_str = """"""
+with open("requirements.txt", 'r' , encoding="utf8") as f:
+    for line in f:
+        if sum([pn in line for pn in package_names]) == 0:
+            write_str += line
+            
+write_str += "--index-url http://rd.thinktronltd.com:28181/simple --trusted-host rd.thinktronltd.com\n"
+for pn in package_names:
+    write_str += pn + "\n"
+
+with open("requirements_new.txt", 'w' , encoding="utf8") as f:
+    f.write(write_str)
